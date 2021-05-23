f556f6f06929c2dd9fc96ee96181aea0c361369b
Mateichuk_Oleksandr <71691100+Meziran@users.noreply.github.com>
2021-05-23 16:09:43 +0300

    5

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
-const allEl = document.querySelector('#categories')
-console.log(`В списке ${allEl.children.length} категории.`)
+const allEl = document.querySelector('#categories');
+console.log(`В списке ${allEl.children.length} категории.`);
 
 // const itemRef = document.querySelectorAll('.item');
 // itemRef.forEach(item => {
@@ -9,6 +9,6 @@ console.log(`В списке ${allEl.children.length} категории.`)
 
 const itemRef = document.querySelectorAll('.item');
 itemRef.forEach(item => {
-    console.log(`Категория: ${item.querySelector('h2').textContent}`);
-    console.log(`Количество элементов:  ${item.querySelectorAll('li').length}`);
+  console.log(`Категория: ${item.querySelector('h2').textContent}`);
+  console.log(`Количество элементов:  ${item.querySelectorAll('li').length}`);
 });
